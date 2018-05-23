a91a544bbf2cfe8dd6ea1be6675fa480391c087a
Corey Mason <corey.mason98@googlemail.com>
2018-05-23 13:46:38 -0700

    Start Environment

--- environment.py
+++ environment.py
@@ -1,4 +1,15 @@
+from multiprocessing import Process, Pipe
+from pysc2.env import sc2_env
+
 
 class Environment:
-    def __init__(self, n_envs):
+    def __init__(self, env_args, n_envs=1):
+        self.env_args = env_args
         self.n_envs = n_envs
+
+    def launch(self):
+        for i in range(self.n_envs):
+
+
+    def step(self, action):
+        print('env step')
