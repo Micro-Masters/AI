d7379d96aa672149b6b8705df11e6be8d8fe1f66
Kirsten <kegillam@ucdavis.edu>
2018-05-27 13:30:41 -0700

    more experimenting

--- a2c.py
+++ a2c.py
@@ -93,58 +93,48 @@ class A2CAgent:
 if __name__ == "__main__":
     # In case of CartPole-v1, maximum length of episode is 500
     env = gym.make('SC2Game-v0')
-    env.setting['map_name'] = 'kill_tanks'
-    '''
+    env.setting['map_name'] = 'killtanks'
     # get size of state and action from environment
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
 
     # make A2C agent
     agent = A2CAgent(state_size, action_size)
-    '''
-	
-    # make A2C agent
-    agent = A2CAgent()
 
     scores, episodes = [], []
 
-    for e in range(EPISODES):
-        done = False
-        score = 0
-        state = env.reset()
-        state = np.reshape(state, [1, state_size])
+#    for e in range(EPISODES):
+#    done = False
+#    score = 0
+    state = env.reset()
+    state = np.reshape(state, [1, state_size])
 
-        while not done:
-            if agent.render:
-                env.render()
-
-            action = agent.get_action(state)
-            next_state, reward, done, info = env.step(action)
-            next_state = np.reshape(next_state, [1, state_size])
-            # if an action make the episode end, then gives penalty of -100
-            reward = reward if not done or score == 499 else -100
-
-            agent.train_model(state, action, reward, next_state, done)
-
-            score += reward
-            state = next_state
-
-            if done:
-                # every episode, plot the play time
-                score = score if score == 500.0 else score + 100
-                scores.append(score)
-                episodes.append(e)
-              	#  pylab.plot(episodes, scores, 'b')
-              	#  pylab.savefig("./save_graph/cartpole_a2c.png")
-                print("episode:", e, "  score:", score)
-
-                # if the mean of scores of last 10 episode is bigger than 490
-                # stop training
-                if np.mean(scores[-min(10, len(scores)):]) > 490:
-                    #sys.exit()
-                    exit()
-
-        # save the model
-        if e % 50 == 0:
-            agent.actor.save_weights("./save_model/cartpole_actor.h5")
-            agent.critic.save_weights("./save_model/cartpole_critic.h5")
+    while not done:
+        if agent.render:
+            env.render()
+         action = agent.get_action(state)
+        next_state, reward, done, info = env.step(action)
+        next_state = np.reshape(next_state, [1, state_size])
+        # if an action make the episode end, then gives penalty of -100
+        #reward = reward if not done or score == 499 else -100
+        #TODO: set rewards
+        agent.train_model(state, action, reward, next_state, done)
+        score += reward
+        state = next_state
+         if done:
+            # every episode, plot the play time
+            score = score if score == 500.0 else score + 100
+            scores.append(score)
+            episodes.append(e)
+          	#  pylab.plot(episodes, scores, 'b')
+          	#  pylab.savefig("./save_graph/cartpole_a2c.png")
+            print("episode:", e, "  score:", score)
+             # if the mean of scores of last 10 episode is bigger than 490
+            # stop training
+            if np.mean(scores[-min(10, len(scores)):]) > 490:
+                #sys.exit()
+                exit()
+     # save the model
+    if e % 50 == 0:
+        agent.actor.save_weights("./save_model/cartpole_actor.h5")
+        agent.critic.save_weights("./save_model/cartpole_critic.h5")
