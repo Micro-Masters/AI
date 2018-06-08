5ed5cacbd7fd53f4fcfe0054a12bf120ae228ad8
Tiffany Yuen <tiffyuen@users.noreply.github.com>
2018-06-08 10:22:47 -0700

    Included PPO codes

--- agent.py
+++ agent.py
@@ -1,84 +1,89 @@
 import tensorflow as tf
 import numpy as np
-import pandas as pd
+from collections import defaultdict
+from tensorflow.contrib import layers
 
-from model import FullyConv
+
+from agent.model import FullyConv
+
 
 class A2CAgent:
     def __init__(
             self, sess, agent_modifier, use_lstm=False, discount=.99, value_loss_coeff=.5,
-            entropy_loss_coeff=.001, learning_rate=1e-4, max_gradient_norm=1):
-        print('A2C init')
+            entropy_loss_coeff=.001, learning_rate=1e-4, max_gradient_norm=1.0):
         self.sess = sess
         self.use_lstm = use_lstm
         self.agent_modifier = agent_modifier
-        self.discount = discount #TODO: use discount from sc2_env observation instead, won't need this class var then
+        self.discount = discount #Could use discount from sc2_env observation instead, won't need this class var then
         self.learning_rate = learning_rate
         self.max_gradient_norm = max_gradient_norm
         self.value_loss_coeff = value_loss_coeff
         self.entropy_loss_coeff = entropy_loss_coeff
         self._build()
-
         self._build_ppo()
-        self.model = model  # TODO: change later
-        # PPO: self.sess = tf.get_default_session()
-        # PPO: self.ppo_model = policy(sess,)
-        self.lr = tf.placeholder(tf.float32, [])
-
-        # TODO: add policy for model
 
     # Build and initialize the TensorFlow graph
     def _build(self):
-        self.action, self.value = self._build_model()
+        self.policy, self.action, self.value = self._build_model()
         self.train_operation = self._build_optimizer()
+        self.sess.run(tf.global_variables_initializer())
 
     def _build_ppo(self):
-        self.action, self.value = self._build_model() # Fixme: check
-        self.train_operation = self._build_ppo_optimizer()
+        self.policy, self.action, self.value = self._build_model()
+        self.train_operation = self._build_optimizer()
 
     # Build TensorFlow action (single action from policy) & value operations
     def _build_model(self):
-        observation_shapes = self.agent_modifier.observation_shapes()
-        self.screen_input = tf.placeholder(tf.float32, observation_shapes['screen'], 'screen input')
-        self.minimap_input = tf.placeholder(tf.float32, observation_shapes['minimap'], 'minimap input')
-        self.nonspatial_input = tf.placeholder(tf.float32, observation_shapes['nonspatial'], 'nonspatial input')
-        self.available_actions_input = tf.placeholder(tf.float32, observation_shapes['available_actions'],
-                                                      'available actions input')
+        # Declare model inputs (of any number of observations) and initialize model
+        observation_shapes = self.agent_modifier.observation_shapes
+        for k, v in observation_shapes.items():
+            observation_shapes[k].insert(0, None)
+        print(observation_shapes['screen'])
+        self.screen_input = tf.placeholder(tf.float32, observation_shapes['screen'], 'screen_input')
+        self.minimap_input = tf.placeholder(tf.float32, observation_shapes['minimap'], 'minimap_input')
+        self.nonspatial_input = tf.placeholder(tf.float32, observation_shapes['nonspatial'], 'nonspatial_input')
+        self.available_mask_input = tf.placeholder(tf.float32, observation_shapes['available_mask'],
+                                                   'available_mask_input')
 
-        model = FullyConv(self.agent_modifier.num_actions, self.use_lstm)
+        self.model = FullyConv(self.agent_modifier.num_actions, self.use_lstm,
+                               self.agent_modifier.observation_data_format)
 
-        policy, value = self.model.build(self.screen_input, self.minimap_input, self.nonspatial_input)
-        # TODO sample action from policy
-        return action, value
+        # Create final action and value operations using model
+        policy, value = self.model.build(self.screen_input, self.minimap_input, self.nonspatial_input,
+                                         self.available_mask_input, *self.agent_modifier.feature_names)
+        action = self.model.sample_action(policy)
+        return policy, action, value
 
     # Build the TensorFlow loss operation
     def _build_optimizer(self):
-        # Tensorflow placeholders
-        returns = tf.placeholder(tf.float32, [None], 'returns')
-        values = tf.placeholder(tf.float32, [None], 'values')
-        advantages = tf.stop_gradient(returns - values)
+        # TensorFlow placeholders and compute advantages
+        fns = tf.placeholder(tf.int32, [None], 'fns')
+        args = {
+            k: tf.placeholder(tf.int32, [None], 'args')
+            for k in self.policy[1].keys()}
+        self.actions = (fns, args)
+        self.returns = tf.placeholder(tf.float32, [None], 'returns')
+        advantages = tf.stop_gradient(self.returns - self.value)
 
         # Create loss TensorFlow operation using placeholders
-        negative_log_policy =  # TODO
+        negative_log_policy = self.model.get_neg_log_prob(self.actions, self.policy)
         policy_loss = tf.reduce_mean(advantages * negative_log_policy)
-        value_loss = self.value_loss_coeff * tf.losses.reduce_mean_squared_error(values, returns)
-        entropy_loss = self.entropy_loss_coeff *  # TODO
+        value_loss = self.value_loss_coeff * tf.reduce_mean(tf.square(self.value - self.returns))
+        entropy_loss = self.entropy_loss_coeff * tf.reduce_mean(self.model.get_entropy(self.policy))
 
+        # Create the final optimizer using loss
         loss = policy_loss + value_loss - entropy_loss
         optimizer = tf.train.RMSPropOptimizer(learning_rate=self.learning_rate, decay=0.99, epsilon=1e-5)
-
-        return tf.layers.optimize_loss(
+        return layers.optimize_loss(
             loss=loss,
             global_step=tf.train.get_global_step(),
             learning_rate=None,
             optimizer=optimizer,
             clip_gradients=self.max_gradient_norm,
-        name="train_operation")
+            name="train_operation")
 
     # modified from baseline ppo2.py
     def _build_ppo_optimizer(self):
-
-        # TODO: add policy for model
 
         # Tensorflow placeholders
         returns = tf.placeholder(tf.float32, [None], 'returns')
@@ -86,12 +91,11 @@ class A2CAgent:
         advantages = tf.stop_gradient(returns - values)
 
         # Create loss TensorFlow operation using placeholders
-        A = self.model.pdtype.sample_placeholder([None])
         R = tf.placeholder(tf.float32, [None])
 
         clip_range = tf.placeholder(tf.float32, [])
         old_neg_log_policy = tf.placeholdeer(tf.float32, [None])
-        negative_log_policy = self.model.pd.neglogp(A)
+        negative_log_policy = self.model.get_neg_log_prob(self.actions, self.policy)
         ratio = tf.exp(old_neg_log_policy - negative_log_policy)
 
         policy_loss1 = -advantages * ratio
@@ -99,14 +103,14 @@ class A2CAgent:
         policy_loss = tf.reduce_mean(tf.maximum(policy_loss1, policy_loss2))
 
         old_value_prev = tf.placeholder(tf.float32, [None])
-        value_prev = self.model.vf
+        value_prev = self.value
         value_prev_clipped = old_value_prev + tf.clip_by_value(value_prev - old_value_prev,
                                                    - clip_range, clip_range)
         value_loss1 = tf.square(value_prev - R)
         value_loss2 = tf.square(value_prev_clipped - R)
         value_loss = 0.5 * tf.reduce_mean(tf.maximum(value_loss1, value_loss2))
 
-        entropy_loss = tf.reduce_mean(self.model.pd.entropy())
+        entropy_loss = tf.reduce_mean(self.model.get_entropy(self.policy))
 
         loss = policy_loss - entropy_loss * entropy_loss_coeff + value_loss * value_loss_coeff
 
@@ -120,21 +124,30 @@ class A2CAgent:
             clip_gradients=self.max_gradient_norm,
             name="train_ppo_operation")
 
+    # Convert policy action syntax to a PySC2 FunctionCall action
+    def convert_action(self, policy_action):
+        return self.agent_modifier.make_action(policy_action)
+
     # Take an observation (n_envs length array) and return the action, value
     def act(self, observation):
-        # TODO also return log prob for action?
-        return self.sess.run([self.action, self.value], feed_dict=self._get_observations_feed(observation))
+        feed_dict = self._get_observation_feed(observation)
+        action, value = self.sess.run([self.action, self.value], feed_dict=feed_dict)
+        return action, value
 
     # Train the model to minimize loss
     def train(self, observations, actions, rewards, dones, values, next_value):
-        returns = self.get_returns(rewards, dones, values, next_value)
-        # TODO: Pass info into feed_dict below
-        self.sess.run(self.train_operation)
+        observations_feed = self._get_observation_feed(observations, train=True)
+        actions_feed = self._get_action_feed(actions)
+        feed_dict = {
+            self.returns: self._get_returns(rewards, dones, values, next_value).flatten(),
+            **observations_feed,
+            **actions_feed
+        }
+        self.sess.run(self.train_operation, feed_dict=feed_dict)
 
-        # Compute return values as return = reward + discount * next value * (0 if done else 1)
-
+    # Compute return values as return = reward + discount * next value * (0 if done else 1)
     def _get_returns(self, rewards, dones, values, next_value):
-        returns = np.zeros((rewards.shape[0], rewards.shape[1]), dtype=np.float32)
+        returns = np.zeros(rewards.shape, dtype=np.float32)
         # Compute the last return using next_value
         returns[-1] = rewards[-1] + self.discount * next_value * (1 - dones[-1])
         # Compute the rest in a loop using values, working backward
@@ -142,60 +155,39 @@ class A2CAgent:
             returns[i] = rewards[i] + self.discount * values[i + 1] * (1 - dones[i])
         return returns
 
-        # Modify PySC2 observations and then return the observation input feed
-
-    def _get_observations_feed(self, observations):
+    # Modify PySC2 observations and then return the observation input feed
+    def _get_observation_feed(self, observation, train=False):
         # Convert PySC2 observations to an array of dictionaries
-        new_observations = self.agent_modifier.modify_observations(observations)
+        if train:
+            new_observations = [self.agent_modifier.modify_observations(obs) for obs in observation]
+            new_observation = new_observations.reshape(-1, *new_observations.shape[1:])
+        else:
+            new_observation = self.agent_modifier.modify_observation(observation)
         # Convert array of dictionaries to array of arrays
-        observations_input = pd.DataFrame(new_observations).values()
+        observation_input = []
+        for obs_dict in new_observation:
+            obs = [np.array(v) for k, v in obs_dict.items()]
+            observation_input.append(obs)
+        observation_input = np.array(observation_input)
+        print(observation_input.shape)
+        observation_input = np.transpose(observation_input)
+        print(observation_input.shape)
+        print(len(observation_input[0]))
+
         return {
-            self.screen_input: observations_input[0],
-            self.minimap_input: observations_input[1],
-            self.nonspatial_input: observations_input[2],
-            self.available_actions: observations_input[3]
+            self.screen_input: observation_input[0],
+            self.minimap_input: observation_input[1],
+            self.nonspatial_input: observation_input[2],
+            self.available_mask_input: observation_input[3]
         }
 
-    # A = self.model.pdtype.sample_placeholder([None])
-    # ADV = tf.placeholder(tf.float32, [None])
-    # R = tf.placeholder(tf.float32, [None])
-    # OLDNEGLOGPAC = tf.placeholdeer(tf.float32, [None])
-    # OLDVPRED = tf.placeholder(tf.float32, [None])
+    # Join actions into single fn list and single arg map
+    def _get_action_feed(self, actions):
+        fns = []
+        args = defaultdict(list)
 
-    # CLIPRANGE = tf.placeholder(tf.float32, [])
-    # neglogpac = self.model.pd.neglogp(A)
-
-    # entropy = tf.reduce_mean(self.model.pd.entropy())
-    # vpred = self.model.vf
-    # vpredclipped = OLDVPRED + tf.clip_by_value(vpred - OLDVPRED,
-    #                                           - CLIPRANGE, CLIPRANGE)
-
-    # vf_losses1 = tf.square(vpred - R)
-    # vf_losses2 = tf.square(vpredclipped - R)
-    # vf_loss = 0.5 * tf.reduce_mean(tf.maximum(vf_losses1, vf_losses2))
-    # ratio = tf.exp(OLDNEGLOGPAC - neglogpac)
-    # pg_losses = -ADV * ratio
-    # pg_losses2 = -ADV * tf.clip_by_value(ratio, 1.0 - CLIPRANGE, 1.0 + CLIPRANGE)
-    # pg_loss = tf.reduce_mean(tf.maximum(pg_losses, pg_losses2))
-
-    # def ppo_train(self, states=None):
-    #         approxkl = .5 * tf.reduce_mean(tf.square(neglogpac - OLDNEGLOGPAC))
-    #
-    #         params = tf.trainable_variables()
-    #         grads = tf.gradients(loss, params)
-    #         if max_grad_norm is not None:
-    #             grads, _grad_norm = tf.clip_by_global_norm(grads, max_grad_norm)
-    #         grads = list(zip(grads, params))
-    #         _ppo_train = trainer.apply_gradients(grads)
-    #
-    #         advs = returns - values
-    #         advs = (advs - advs.mean()) / (advs.std() + 1e-8)
-    #
-    #         obs, returns, masks, actions, values, neglogpacs, states, epinfos = runner.run()
-    #
-    #         td_map = {train_model.X: obs, A: actions, ADV: advs, R: returns, LR: LR,
-    #                   CLIPRANGE: CLIPRANGE, OLDNEGLOGPAC: neglogpacs, OLDVPRED: values}
-    #
-    #         return sess.run(
-    #             [pg_loss, vf_loss, entropy, approxkl, clipfrac, _ppo_train],
-    #             td_map)[:-1]
+        for fn, arg in actions:
+            fns.append(fn)
+            for k, v in arg:
+                args[k].append(v)
+        return {self.actions: (np.array(fns).flatten(), args)}
